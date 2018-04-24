2ee6f14152e738e1d6ddcd3b02e8d0f27ba7547c
Dodo33 <dodo.33@gmx.com>
2018-04-24 18:09:55 +0200

    Forgot to change the version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def parse_requirements(filename):
 
 
 setup(name = 'alchemist_lib',
-      version = '0.5',
+      version = '0.6',
       description = 'Automatic trading library for cryptocurrencies.',
       long_description = read("README.rst"),
       url = 'https://github.com/Dodo33/alchemist-lib',
